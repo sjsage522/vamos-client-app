7ec320dda1e09a16ee288084ff45b0673137f169
shinjunseok <sjsage522@gmail.com>
2021-10-27 16:06:31 +0900

    update : 클래스형 컴포넌트 구현

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,18 @@
 import './NotFound.css';
 
-export default function() {
-    return (
-        <div id="notfound">
-            <div className="notfound">
-                <div className="notfound-404">
-                    <h3>Oops! Page not found</h3>
-                    <h1><span>4</span><span>0</span><span>4</span></h1>
+export default class NotFound {
+
+    render() {
+        return (
+            <div id="notfound">
+                <div className="notfound">
+                    <div className="notfound-404">
+                        <h3>Oops! Page not found</h3>
+                        <h1><span>4</span><span>0</span><span>4</span></h1>
+                    </div>
+                    <h2>we are sorry, but the page you requested was not found</h2>
                 </div>
-                <h2>we are sorry, but the page you requested was not found</h2>
             </div>
-        </div>
-    );
+        );
+    }
 }
