43ece85c5031f7e75dc56b6875b6769d18649c67
shinjunseok <sjsage522@gmail.com>
2021-10-28 15:29:49 +0900

    fix : 리다이렉트 url 변경

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = 'https://api.vamos.pw/api';
 export const API_BASE_URL = 'http://localhost:8080/api'
-export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/location'
+export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const NAVER_AUTH_URL = API_BASE_URL + '/oauth2/authorize/naver?redirect_uri=' + OAUTH2_REDIRECT_URI;
