8ed1ee5c3ed53f610a1bad996f30837a4feac123
shinjunseok <sjsage522@gmail.com>
2021-10-29 17:44:25 +0900

    add : 업로드 후 경과시간 계산 로직 구현

---
+++ src/util/TimeUtils.js
@@ -0,0 +1,17 @@
+export function getLastTime(compare, now) {
+    let result = Math.floor((now - compare) / 1000 / 60 / 60);
+    if (result > 24) { //24시간 이상
+        result = Math.floor(result / 24);
+        if (result > 7) { //7일 이상
+            result = `${compare.getFullYear()}. ${compare.getMonth() + 1} .${compare.getDay()}`
+            // ex) 2021. 10. 27
+        } else { //7일 이내
+            result = result + '일전' // ex) 5일전
+        }
+    } else if (result === 0) { //1시간 미만
+        result = Math.floor((now - compare) / 1000 / 60) + '분전';
+    } else { //1시간 이상 24시간 미만
+        result = result + '시간전';
+    }
+    return result;
+}
