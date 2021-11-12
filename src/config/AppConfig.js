ef65737e1e44d24c39f96e58a880f0b27b8c5086
shinjunseok <sjsage522@gmail.com>
2021-11-12 11:35:42 +0900

    update : add dependency

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -1,5 +1,5 @@
-export const API_BASE_URL = 'https://api.vamos.pw/api';
-//export const API_BASE_URL = 'http://localhost:8080/api'
+// export const API_BASE_URL = 'https://api.vamos.pw/api';
+export const API_BASE_URL = 'http://localhost:8080/api'
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
